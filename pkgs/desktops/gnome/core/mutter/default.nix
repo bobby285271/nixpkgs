afae54aa55bec6731d276fa1bd8e590d9576993f
Bobby Rong <rjl931189261@126.com>
2023-03-08 02:34:13 +0000

    gnome.mutter: 44.beta → 44.rc
    https://gitlab.gnome.org/GNOME/mutter/-/compare/44.beta...44.rc

--- pkgs/desktops/gnome/core/mutter/default.nix
+++ pkgs/desktops/gnome/core/mutter/default.nix
@@ -52,13 +52,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "mutter";
-  version = "44.beta";
+  version = "44.rc";
 
   outputs = [ "out" "dev" "man" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/mutter/${lib.versions.major finalAttrs.version}/mutter-${finalAttrs.version}.tar.xz";
-    sha256 = "+Lgh89e9o9o5yzygwDg84sn/HCqzFNyJXoRyKBVxKkc=";
+    sha256 = "aQynddetFbm1DA48J2w+xH/Fvi+AVhLDZ1guay3jgls=";
   };
 
   mesonFlags = [
