d4935c3fc89ea54ae45b213418af74e6c1a919ee
Bobby Rong <rjl931189261@126.com>
2023-06-01 21:30:29 +0800

    cinnamon.cinnamon-settings-daemon: 5.6.2 -> 5.8.0
    https://github.com/linuxmint/cinnamon-settings-daemon/compare/5.6.2...5.8.0

--- pkgs/desktops/cinnamon/cinnamon-settings-daemon/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-settings-daemon/default.nix
@@ -32,18 +32,17 @@
 
 stdenv.mkDerivation rec {
   pname = "cinnamon-settings-daemon";
-  version = "5.6.2";
+  version = "5.8.0";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-IqYfHMjKe7gVsM6HgihQMNkcXSYBOft1lamXOLa1Y8k=";
+    hash = "sha256-siWsza3SXs+GlHGqXR2Q3436Q4u5WVHqeqnQtzEg2tE=";
   };
 
   patches = [
     ./csd-backlight-helper-fix.patch
-    ./use-sane-install-dir.patch
   ];
 
   buildInputs = [
