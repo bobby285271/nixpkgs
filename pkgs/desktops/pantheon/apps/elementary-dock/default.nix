92cfe24ba26318ae756b5eaebea790fbb8c91f2b
Bobby Rong <rjl931189261@126.com>
2023-06-15 23:01:09 +0800

    pantheon.elementary-dock: unstable-2021-05-07 -> unstable-2023-06-15

--- pkgs/desktops/pantheon/apps/elementary-dock/default.nix
+++ pkgs/desktops/pantheon/apps/elementary-dock/default.nix
@@ -1,77 +1,46 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, vala
-, atk
-, cairo
-, dconf
-, glib
-, gtk3
-, libwnck
-, libX11
-, libXfixes
-, libXi
-, pango
-, pkg-config
-, libxml2
-, bamf
-, gdk-pixbuf
-, libdbusmenu-gtk3
-, gnome-menus
-, libgee
-, wrapGAppsHook
 , meson
 , ninja
-, granite
+, pkg-config
+, python3
+, vala
+, wrapGAppsHook4
+, glib
+, gtk4
 }:
 
 stdenv.mkDerivation rec {
   pname = "elementary-dock";
-  version = "unstable-2021-05-07";
+  version = "unstable-2023-06-15";
 
   outputs = [ "out" "dev" ];
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = "dock";
-    rev = "113c3b0bc7744501d2101dd7afc1ef21ba66b326";
-    sha256 = "sha256-YlvdB02/hUGaDyHIHy21bgloHyVy3vHcanyNKnp3YbM=";
+    rev = "fc71528d7be78c794eba76b0b3107585ba1d1769"; # tintou/container
+    sha256 = "sha256-I2623oHFH71s9hnjxuG54zr81dNC2+9eTVDHS5rEZUE=";
   };
 
   nativeBuildInputs = [
     meson
     ninja
-    libxml2 # xmllint
     pkg-config
+    python3
     vala
-    wrapGAppsHook
+    wrapGAppsHook4
   ];
 
   buildInputs = [
-    atk
-    bamf
-    cairo
-    gdk-pixbuf
     glib
-    gnome-menus
-    dconf
-    granite
-    gtk3
-    libX11
-    libXfixes
-    libXi
-    libdbusmenu-gtk3
-    libgee
-    libwnck
-    pango
+    gtk4
   ];
 
-  postInstall = ''
-    # elementary/dock/master is missing a Meson post
-    # install script that does this. This has been
-    # resolved after the dock rewrite (the `main` branch).
-    # https://github.com/elementary/default-settings/issues/267
-    glib-compile-schemas $out/share/glib-2.0/schemas
+  postPatch = ''
+    chmod +x meson/post_install.py
+    patchShebangs meson/post_install.py
   '';
 
   meta = with lib; {
