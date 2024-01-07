2fca1f2101494f37854a1074d7164820fe643a10
Bobby Rong <rjl931189261@126.com>
2024-01-07 14:07:19 +0000

    at-spi2-core: 2.50.0 -> 2.51.0
    https://gitlab.gnome.org/GNOME/at-spi2-core/-/compare/2.50.0...2.51.0

--- pkgs/development/libraries/at-spi2-core/default.nix
+++ pkgs/development/libraries/at-spi2-core/default.nix
@@ -23,13 +23,13 @@
 
 stdenv.mkDerivation rec {
   pname = "at-spi2-core";
-  version = "2.50.0";
+  version = "2.51.0";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "6fWoyCNcndljshcd6RIDARKcZ33ekzlV4d9hi5ScStw=";
+    sha256 = "jdB8YWDjEV9Pd+IgWWNEne9oIqPchdSVxds4n1ZmMDc=";
   };
 
   nativeBuildInputs = [
