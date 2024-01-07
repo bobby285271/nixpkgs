46d577b35cf93b6ab8bc296f8b0b45d7f018a9a2
Bobby Rong <rjl931189261@126.com>
2024-01-07 14:08:01 +0000

    gnome.gnome-shell-extensions: 45.2 -> 46.alpha
    https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/compare/45.2...46.alpha

--- pkgs/desktops/gnome/core/gnome-shell-extensions/default.nix
+++ pkgs/desktops/gnome/core/gnome-shell-extensions/default.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-shell-extensions";
-  version = "45.2";
+  version = "46.alpha";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-shell-extensions/${lib.versions.major finalAttrs.version}/gnome-shell-extensions-${finalAttrs.version}.tar.xz";
-    sha256 = "7jL2OHotGK2/96lWaJvHR4ZrSocS1zeQwAKr6uTMqq8=";
+    sha256 = "qAuH0zrfASBFFFtym6kY1XLNwejeCR+7lz5X23dJW6c=";
   };
 
   patches = [
