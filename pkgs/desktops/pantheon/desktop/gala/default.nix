925d35b5c5065a741f72c2496736cfeaab9aa5a8
Bobby Rong <rjl931189261@126.com>
2024-06-05 20:41:24 +0800

    amend! pantheon.gala: 7.1.3 -> 7.1.3-unstable-2024-05-05
    pantheon.gala: 7.1.3 -> 7.1.3-unstable-2024-05-31
    
    https://github.com/elementary/gala/compare/7.1.3...d5e89a9fd1bdd3fdadfbd3c9a562e738684d048f

--- pkgs/desktops/pantheon/desktop/gala/default.nix
+++ pkgs/desktops/pantheon/desktop/gala/default.nix
@@ -1,7 +1,6 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, fetchpatch
 , nix-update-script
 , pkg-config
 , meson
@@ -28,33 +27,19 @@
 
 stdenv.mkDerivation rec {
   pname = "gala";
-  version = "7.1.3-unstable-2024-05-05";
+  version = "7.1.3-unstable-2024-05-31";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
-    rev = "196d2498a257d8af48221b1999639bcce8b80eb1";
-    sha256 = "sha256-UJ8T/oGUwSKV1L8sub8tpMLeeAfyNEk8PCqGnCGjyFA=";
+    rev = "d5e89a9fd1bdd3fdadfbd3c9a562e738684d048f";
+    sha256 = "sha256-YOAdufrnoHjICCP2e91Cl1BnS1QNbQ9yWzW140eK5Po=";
   };
 
   patches = [
     # We look for plugins in `/run/current-system/sw/lib/` because
     # there are multiple plugin providers (e.g. gala and wingpanel).
     ./plugins-dir.patch
-
-    # InternalUtils: Fix window placement
-    # https://github.com/elementary/gala/pull/1913
-    (fetchpatch {
-      url = "https://github.com/elementary/gala/commit/2d30bee678788c5a853721d16b5b39c997b23c02.patch";
-      hash = "sha256-vhGFaLpJZFx1VTfjY1BahQiOUvBPi0dBSXLGhYc7r8A=";
-    })
-
-    # BackgroundManager: Don't set visible
-    # https://github.com/elementary/gala/pull/1910
-    (fetchpatch {
-      url = "https://github.com/elementary/gala/commit/e0e8bb7a70069719eb822aabd46dc66a38996e9e.patch";
-      hash = "sha256-eYgvbbEVSjvwmH0AZJMKG5Kfv45tuYt85PwAxBEl468=";
-    })
   ];
 
   nativeBuildInputs = [
