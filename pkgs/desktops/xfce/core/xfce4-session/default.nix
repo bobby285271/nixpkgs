8dd0db1374d07976ef0e40d96164e9e05e94a1e5
Bobby Rong <rjl931189261@126.com>
2024-10-16 12:15:29 +0000

    xfce.xfce4-session: 4.18.4 -> 4.19.2
    https://gitlab.xfce.org/xfce/xfce4-session/-/compare/xfce4-session-4.18.4...xfce4-session-4.19.2

--- pkgs/desktops/xfce/core/xfce4-session/default.nix
+++ pkgs/desktops/xfce/core/xfce4-session/default.nix
@@ -4,9 +4,11 @@
 , exo
 , libxfce4util
 , libxfce4ui
+, libxfce4windowing
 , xfconf
 , iceauth
 , gtk3
+, gtk-layer-shell
 , glib
 , libwnck
 , xfce4-session
@@ -15,23 +17,28 @@
 mkXfceDerivation {
   category = "xfce";
   pname = "xfce4-session";
-  version = "4.18.4";
+  version = "4.19.2";
 
-  sha256 = "sha256-YxIHxb8mRggHLJ9TQ+KGb9qjt+DMZrxMn+oFuFRL8GI=";
+  sha256 = "sha256-249828gMCjZwVEH3pKQ4IvrI9JRkpF2MSzkvDI1GcTc=";
 
   buildInputs = [
     exo
     gtk3
+    gtk-layer-shell
     glib
     libxfce4ui
     libxfce4util
+    libxfce4windowing
     libwnck
     xfconf
     polkit
     iceauth
   ];
 
-  configureFlags = [ "--with-xsession-prefix=${placeholder "out"}" ];
+  configureFlags = [
+    "--with-xsession-prefix=${placeholder "out"}"
+    "--with-wayland-session-prefix=${placeholder "out"}"
+  ];
 
   passthru.xinitrc = "${xfce4-session}/etc/xdg/xfce4/xinitrc";
 
