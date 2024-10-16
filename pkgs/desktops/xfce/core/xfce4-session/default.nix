6a0cb8d1cefa304841c1cd0be82f127f4bf2e74a
Bobby Rong <rjl931189261@126.com>
2024-10-16 12:15:29 +0000

    xfce.xfce4-session: 4.18.4 -> 4.19.2
    https://gitlab.xfce.org/xfce/xfce4-session/-/compare/xfce4-session-4.18.4...xfce4-session-4.19.2

--- pkgs/desktops/xfce/core/xfce4-session/default.nix
+++ pkgs/desktops/xfce/core/xfce4-session/default.nix
@@ -4,9 +4,11 @@
 , exo
 , libxfce4util
 , libxfce4ui
+, libxfce4windowing
 , xfconf
 , iceauth
 , gtk3
+, gtk-layer-shell
 , glib
 , libwnck
 , xfce4-session
@@ -15,25 +17,44 @@
 mkXfceDerivation {
   category = "xfce";
   pname = "xfce4-session";
-  version = "4.18.4";
+  version = "4.19.2";
+  rev = "7333c5659021e2d1f0da67f895e4066068ac948d";
 
-  sha256 = "sha256-YxIHxb8mRggHLJ9TQ+KGb9qjt+DMZrxMn+oFuFRL8GI=";
+  outputs = [ "waylandsession" ];
+
+  sha256 = "sha256-6IG8C4Nzzm7HZc0SExgzYA2N+k9wwH8q7yjjMY3pfLU=";
 
   buildInputs = [
     exo
     gtk3
+    gtk-layer-shell
     glib
     libxfce4ui
     libxfce4util
+    libxfce4windowing
     libwnck
     xfconf
     polkit
     iceauth
   ];
 
-  configureFlags = [ "--with-xsession-prefix=${placeholder "out"}" ];
+  postInstall = ''
+    # To ensure the desktop file has consistent l10n status with the x11 session,
+    # we copy the original (i.e. untranslated) file here.
+    mkdir -p $waylandsession/share/wayland-sessions
+    cp xfce-wayland.desktop.in $waylandsession/share/wayland-sessions/xfce-wayland.desktop
+  '';
 
-  passthru.xinitrc = "${xfce4-session}/etc/xdg/xfce4/xinitrc";
+  configureFlags = [
+    "--with-xsession-prefix=${placeholder "out"}"
+    "--with-wayland-session-prefix=${placeholder "out"}"
+  ];
+
+  passthru = {
+    # The x11 session file is manually created in the Xfce NixOS module.
+    providedSessions = [ "xfce-wayland" ];
+    xinitrc = "${xfce4-session}/etc/xdg/xfce4/xinitrc";
+  };
 
   meta = with lib; {
     description = "Session manager for Xfce";
