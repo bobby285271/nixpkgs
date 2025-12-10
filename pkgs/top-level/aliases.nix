5eea3ffe3c752b21d59f67f265754478a893586f
Bobby Rong <rjl931189261@126.com>
2025-12-10 21:08:42 +0800

    pantheon.gnome-settings-daemon: 46.0 -> 48.1
    https://github.com/GNOME/gnome-settings-daemon/blob/48.1/NEWS

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -686,6 +686,7 @@ mapAliases {
   gnome-passwordsafe = throw "'gnome-passwordsafe' has been renamed to/replaced by 'gnome-secrets'"; # Converted to throw 2025-10-27
   gnome-recipes = throw "'gnome-recipes' has been removed due to lack of upstream maintenance and dependency on insecure libraries"; # Added 2025-09-06
   gnome-resources = throw "'gnome-resources' has been renamed to/replaced by 'resources'"; # Converted to throw 2025-10-27
+  gnome-settings-daemon46 = throw "'gnome-settings-daemon46' has been removed, no longer used by Pantheon"; # Added 2025-12-10
   gnu-cobol = throw "'gnu-cobol' has been renamed to/replaced by 'gnucobol'"; # Converted to throw 2025-10-27
   gnubik = throw "'gnubik' has been removed due to lack of maintenance upstream and its dependency on GTK 2"; # Added 2025-09-16
   go-thumbnailer = throw "'go-thumbnailer' has been renamed to/replaced by 'thud'"; # Converted to throw 2025-10-27
