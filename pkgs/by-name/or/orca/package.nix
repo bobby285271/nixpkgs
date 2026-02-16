173ec1ee3c14512bffbacd7b6dc5c0231b3930dd
Bobby Rong <rjl931189261@126.com>
2026-02-16 02:30:37 +0000

    orca: 49.5 → 50.beta.7
    https://gitlab.gnome.org/GNOME/orca/-/compare/49.5...50.beta.7

--- pkgs/by-name/or/orca/package.nix
+++ pkgs/by-name/or/orca/package.nix
@@ -31,13 +31,13 @@
 
 python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "orca";
-  version = "49.5";
+  version = "50.beta.7";
 
   pyproject = false;
 
   src = fetchurl {
     url = "mirror://gnome/sources/orca/${lib.versions.major finalAttrs.version}/orca-${finalAttrs.version}.tar.xz";
-    hash = "sha256-U99BVYMZ6XwehK1gSYmVegK10P9TFBkZDwWH6mslYDQ=";
+    hash = "sha256-6LQuQGtgZLXtq16JUUQtlv7TJ3RLATDkZ1w7bTpJedY=";
   };
 
   patches = [
