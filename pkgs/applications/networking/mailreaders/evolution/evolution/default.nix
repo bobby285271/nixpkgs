ae3da7f8c8806270781c1728f76e277468afd850
Bobby Rong <rjl931189261@126.com>
2026-02-16 02:22:03 +0000

    evolution: 3.58.3 → 3.59.2
    https://gitlab.gnome.org/GNOME/evolution/-/compare/3.58.3...3.59.2

--- pkgs/applications/networking/mailreaders/evolution/evolution/default.nix
+++ pkgs/applications/networking/mailreaders/evolution/evolution/default.nix
@@ -49,7 +49,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "evolution";
-  version = "3.58.3";
+  version = "3.59.2";
 
   outputs = [
     "out"
@@ -58,7 +58,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/evolution/${lib.versions.majorMinor finalAttrs.version}/evolution-${finalAttrs.version}.tar.xz";
-    hash = "sha256-XQG3mlvr3y06mHG2E1CTnPJQH26gPCQVQ/dEqShtTXk=";
+    hash = "sha256-RzYbbQI+DosIb1lsXQaqAPCE7dsX7vaNYpijU8rrFnQ=";
   };
 
   nativeBuildInputs = [
