fde046c37d8a2ec2892363a679b4b6bc1b530fbe
Bobby Rong <rjl931189261@126.com>
2026-02-16 02:24:47 +0000

    gnome-nibbles: 4.4.2 → 4.5.0
    https://gitlab.gnome.org/GNOME/gnome-nibbles/-/compare/4.4.2...4.5.0

--- pkgs/by-name/gn/gnome-nibbles/package.nix
+++ pkgs/by-name/gn/gnome-nibbles/package.nix
@@ -21,11 +21,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-nibbles";
-  version = "4.4.2";
+  version = "4.5.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-nibbles/${lib.versions.majorMinor finalAttrs.version}/gnome-nibbles-${finalAttrs.version}.tar.xz";
-    hash = "sha256-FuBgKHBkamZTh2y8Ye4j92NAwmsSyeicfDASCEUEQVU=";
+    hash = "sha256-kvLS7AoztoFMRitGjNDoMIXgY9heMR5qDJqmm2L/6RQ=";
   };
 
   nativeBuildInputs = [
