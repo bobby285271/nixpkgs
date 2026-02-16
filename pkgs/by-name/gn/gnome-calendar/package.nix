d5b8eea917b9fb1784505f23438f54896a221330
Bobby Rong <rjl931189261@126.com>
2026-02-16 02:23:45 +0000

    gnome-calendar: 49.1 → 50.beta
    https://gitlab.gnome.org/GNOME/gnome-calendar/-/compare/49.1...50.beta

--- pkgs/by-name/gn/gnome-calendar/package.nix
+++ pkgs/by-name/gn/gnome-calendar/package.nix
@@ -24,11 +24,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-calendar";
-  version = "49.1";
+  version = "50.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-calendar/${lib.versions.major finalAttrs.version}/gnome-calendar-${finalAttrs.version}.tar.xz";
-    hash = "sha256-DBEVqylNUyxMGfVs4Neu5T+OoysUMCCd0dntpHqD0sI=";
+    hash = "sha256-/saY+Sn4pInEZ6ukjoTIJ2piXtBHNarS7/NZs6X7bz0=";
   };
 
   nativeBuildInputs = [
