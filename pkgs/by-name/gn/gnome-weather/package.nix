92e700abc743753cc27f00cf1ef9498519aae517
Bobby Rong <rjl931189261@126.com>
2026-02-16 02:27:17 +0000

    gnome-weather: 49.0 → 50.beta
    https://gitlab.gnome.org/GNOME/gnome-weather/-/compare/49.0...50.beta

--- pkgs/by-name/gn/gnome-weather/package.nix
+++ pkgs/by-name/gn/gnome-weather/package.nix
@@ -22,11 +22,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-weather";
-  version = "49.0";
+  version = "50.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-weather/${lib.versions.major finalAttrs.version}/gnome-weather-${finalAttrs.version}.tar.xz";
-    hash = "sha256-7h92uF66nbDI1cAgQanYXs3SKrtexrs/8yIlkpwPzl8=";
+    hash = "sha256-+f/CYqqyJn2fuTzg1rONTCWhuBvasmZ1Mt+RYNKrPS4=";
   };
 
   nativeBuildInputs = [
