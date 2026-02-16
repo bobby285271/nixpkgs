558f0f4bc89701c67c6ef38f6c742396b1ef5508
Bobby Rong <rjl931189261@126.com>
2026-02-16 02:31:02 +0000

    showtime: 49.1 → 50.beta
    https://gitlab.gnome.org/GNOME/showtime/-/compare/49.1...50.beta

--- pkgs/by-name/sh/showtime/package.nix
+++ pkgs/by-name/sh/showtime/package.nix
@@ -19,12 +19,12 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "showtime";
-  version = "49.1";
+  version = "50.beta";
   pyproject = false;
 
   src = fetchurl {
     url = "mirror://gnome/sources/showtime/${lib.versions.major finalAttrs.version}/showtime-${finalAttrs.version}.tar.xz";
-    hash = "sha256-iu+7DiAJx6HNRKuAGwbKN19+loPwKaBS64b7Qzp4U5M=";
+    hash = "sha256-ezJJj1y6UWuOYMVXNmnHonqCKSrxepi90GwvaMguoBM=";
   };
 
   strictDeps = true;
