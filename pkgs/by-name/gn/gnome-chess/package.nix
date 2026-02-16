a7f4a73aed55aa9e9f56a681def5481ddc126984
Bobby Rong <rjl931189261@126.com>
2026-02-16 02:23:52 +0000

    gnome-chess: 49.2 → 50.beta
    https://gitlab.gnome.org/GNOME/gnome-chess/-/compare/49.2...50.beta

--- pkgs/by-name/gn/gnome-chess/package.nix
+++ pkgs/by-name/gn/gnome-chess/package.nix
@@ -22,11 +22,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-chess";
-  version = "49.2";
+  version = "50.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-chess/${lib.versions.major finalAttrs.version}/gnome-chess-${finalAttrs.version}.tar.xz";
-    hash = "sha256-xAoABKRz/nSawvpPrZjbZBGNGPn9msAu7Po2TwPb6bA=";
+    hash = "sha256-bBNnuRIcEZLGKfuo819SCmJ8EPH2YJaq3SYCYZrNyPU=";
   };
 
   nativeBuildInputs = [
